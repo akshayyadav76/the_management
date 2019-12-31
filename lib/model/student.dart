7c94e251a2447c9b475bacf537ba44c9af68e114
akshay yadav <akshayyadav7613@gmail.com>
2019-12-31 16:47:39 +0530

    initial model class of student...

--- lib/model/student.dart
+++ lib/model/student.dart
@@ -1,1 +1,39 @@
-import '';
+import 'package:flutter/foundation.dart';
+
+class Student {
+
+  String rollNumberId;
+  String studentName;
+  String fatherName;
+  String motherName;
+  String section;
+  String mentor;
+  String category;
+  String dob;
+  String localAddress;
+  String permanentAddress;
+  String emailId;
+  String photo;
+  int contactNo;
+  int parentNo;
+
+  Student({
+   @required this.rollNumberId,
+   @required this.studentName,
+    this.fatherName,
+    this.motherName,
+   @required this.section,
+   @required this.mentor,
+    this.category,
+    this.dob,
+    this.localAddress,
+    this.permanentAddress,
+   @required this.emailId,
+   @required this.photo,
+   @required this.contactNo,
+    this.parentNo,
+});
+
+}
+
+
