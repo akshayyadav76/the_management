36499aeff87f603396be87f0ec53f5b05a003b84
akshay yadav <akshayyadav7613@gmail.com>
2020-01-01 17:11:36 +0530

    error in listtile..

--- lib/widget/student_tile.dart
+++ lib/widget/student_tile.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class StudentTile extends StatelessWidget {
@@ -17,21 +18,66 @@ class StudentTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(studentName),
-      leading: CircleAvatar(backgroundImage: AssetImage(photo),),
-//      trailing: widget(
-//        child: Row(
-//            children: <Widget>[
-//              IconButton(icon: Icon(Icons.favorite_border), onPressed: () {},),
-//              IconButton(icon: Icon(Icons.more_vert), onPressed: () {},)
-//            ]),
-//      ),
-      subtitle: Row(children: <Widget>[
-        Text('Section: $section'),
-        Text('Mentor: $mentor'),
-      ],),
+   final dd= MediaQuery.of(context).size.width;
+    return Card(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          CircleAvatar(backgroundImage: AssetImage(photo),),
+          Column(children: <Widget>[
+            Text(studentName),
+           Row(children: <Widget>[
+                Text('Section: $section'),
+                Text('Mentor: $mentor'),
+              ],),
+          ],),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              SizedBox(width: 30,),
+              Container(
+                  height: 40,
+                  width: 10,
+                  child: IconButton(icon: Icon(Icons.favorite_border), onPressed: () {},)),
+              IconButton(icon: Icon(Icons.more_vert), onPressed: () {},)
+            ],
+          ),
 
+        ],
+      ),
     );
+
+
+//
+//      Card(
+//      child: Row(
+//        mainAxisSize: MainAxisSize.min,
+//        children: <Widget>[
+//          Container(
+//            width: dd*0.8,
+//            child: ListTile(
+//              title: Text(studentName),
+//              leading: CircleAvatar(backgroundImage: AssetImage(photo),),
+//              trailing:
+//
+//
+//                      //mainAxisAlignment: MainAxisAlignment.end,
+//
+//                        IconButton(icon: Icon(Icons.favorite_border), onPressed: () {},),
+//                       //
+//              subtitle: Row(children: <Widget>[
+//                Text('Section: $section'),
+//                Text('Mentor: $mentor'),
+//              ],),
+//              onTap: (){},
+//
+//            ),
+//          ),
+//          Container(
+//              width: dd*0.2,
+//              child: IconButton(icon: Icon(Icons.more_vert), onPressed: () {},))
+//        ],
+//      ),
+//    );
   }
 }
